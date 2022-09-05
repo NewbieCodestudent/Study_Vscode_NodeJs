b4baba6d2c74b2a99e788977f90bd8eb583f07a2
LeeJaeSeok <107614339+NewbieCodestudent@users.noreply.github.com>
2022-09-05 13:59:45 +0900

    update code

--- 생활코딩_Nodejs/main.js
+++ 생활코딩_Nodejs/main.js
@@ -1,19 +1,19 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-
+ 
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var title = queryData.id;
     if(_url == '/'){
-      title = "Welcome";
+      title = 'Welcome';
     }
     if(_url == '/favicon.ico'){
       return response.writeHead(404);
     }
     response.writeHead(200);
-    fs.readFile = (`data/${queryData.id}`, 'utf8', function(err, descripton){
+    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
       var template = `
       <!doctype html>
       <html>
@@ -29,12 +29,13 @@ var app = http.createServer(function(request,response){
           <li><a href="/?id=JavaScript">JavaScript</a></li>
         </ul>
         <h2>${title}</h2>
-        <p>${descripton}</p>
+        <p>${description}</p>
       </body>
       </html>
       `;
       response.end(template);
     })
+ 
  
 });
 app.listen(3000);
